43242ea4b556063e0e134f355115fc4072866f17
gaybullayevumid <umidgaybullayev955@gmail.com>
2024-08-07 16:14:41 +0500

    feat: 3-dars ro'yhatlar va qatorlar amaliy

--- 3-lists-tuples/main.py
+++ 3-lists-tuples/main.py
@@ -86,11 +86,73 @@
 # sorted_list = sorted(davlatlar, reverse=False )
 # print(sorted_list)
 
-raqamlar = [2, 8, 1, 9]
+# raqamlar = [2, 8, 1, 9]
 # eng_katta = max(raqamlar)
 # eng_kichik = min(raqamlar)
 # print(f"Raqamlar ro'yhatimiz ichida eng kattasi {eng_katta}")
 # print(f"Raqamlar ro'yhatimiz ichida eng kichigi {eng_kichik}")
 
-yigindi = sum(raqamlar)
-print(yigindi)
+# yigindi = sum(raqamlar)
+# print(yigindi)
+
+# sonlar = list(range(10)) # 0dan 10gacha oraliqda sonlar yaratadi
+# sonlar.sort(reverse=True)
+# print(sonlar)
+
+# juft_sonlar = list(range(0,20,2)) # 0 dan 20 gacha 2 qadam bilan
+# toq_sonlar = list(range(1,20,2))  # 1 dan 20 gacha 2 qadam bilan
+# print("Juft sonlar: ", juft_sonlar)
+# print("Toq sonlar: ", toq_sonlar)
+
+# # Oddiy tuplam yaratish
+# my_tuple = (1, 2, 3)
+# print(my_tuple)  # (1, 2, 3)
+
+# # list dan tuplam yaratish
+# another_tuple = tuple([4, 5, 6])
+# print(another_tuple)  # (4, 5, 6)
+
+# # bo'sh tuplam yaratish
+# empty_tuple = ()
+# print(empty_tuple)  # ()
+
+# # bitta elementli tuplam yaratish uchun vergul qo'yish kerak
+# single_element_tuple = (1,)
+# print(single_element_tuple)  # (1,)
+
+# my_tuple = (10, 20, 30, 40, 50)
+# print(my_tuple[0])  # 10
+# print(my_tuple[2])  # 30
+# print(my_tuple[-1]) # 50 (oxirgi element)
+
+# my_tuple = (1, 2, 3, 4, 5)
+# print(len(my_tuple))  # 5
+
+# toys = ('bus','car','bear','dino','snake','lizard') # o'zgarmas ro'yxat
+# toys = list(toys) # o'zgarmas ro'yxatni oddiy ro'yxatga (List) aylantiramiz
+# # Ro'yxatga o'zgartirishlar kiritamiz
+# toys.append('dragon')
+# toys.remove('bus')
+# toys[1] = 'mcqueen'
+# toys = tuple(toys) # Ro'yxatni qaytadan o'zgarmas ro'yxatga (Tuple) aylantiramiz
+# print(toys)
+
+# animals = ("rabit","tigr","lion")
+# print(animals)
+
+# animals = ("rabit","tigr","lion")
+# animals = list(animals)
+# print(animals[0])
+
+
+# animals = ("rabit","tigr","lion")
+# animals = list(animals)
+# print(len(animals))
+
+# animals = ("rabit","tigr","lion","dog","cat")
+
+
+# ismlar = ["Behruz", "Shahrizoda", "Hamidjon"]
+# print(f"Salom {ismlar[0].upper()}, bugun birga dars qilamizmi")
+# print(f"Salom {ismlar[1].upper()}, bugun birga dars qilamizmi")
+# print(f"Salom {ismlar[2].upper()}, bugun birga dars qilamizmi")
