385e10db7689ae980bb28852ccadb68a91c17367
gaybullayevumid <umidgaybullayev955@gmail.com>
2024-08-08 18:09:58 +0500

    docs: 4-dars to;plamlar

--- 4-sets/main.py
+++ 4-sets/main.py
@@ -1,4 +1,17 @@
-my_set = {1, 2, 3, 4, 5}
+# Bo'sh set yaratish
+# my_set = set()
 
-my_set.discard(7)
-print(my_set)  # {1, 2, 3, 4, 5}
+# Elementlar bilan set yaratish
+# my_set = {1, 2, 3, 4, 5}
+# my_set.add("Hello")
+# my_set.update([6, 7, 8])
+# print(my_set)  # {1, 2, 4, 5, 6, 7, 8}
+# print(my_set)  # {1, 2, 3, 4, 5}
+# my_set.remove(9)
+# my_set.discard(9)
+# print(my_set)  # {1, 2, 4, 5}
+
+# set1 = {1, 2, 3, 4}
+# set2 = {3, 4, 5, 6}
+set3 = {3, 4, 5, 6}
+print(set1.intersection(set2, set3))  # {3, 4}
